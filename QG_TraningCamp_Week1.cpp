989d041ab1374f731aa4d85dec6561b3615e8402
HSyuf <2969359664@qq.com>
2023-03-23 22:38:43 +0800

    第一周周记写完版本

---
+++ QG_TraningCamp_Week1.cpp
@@ -0,0 +1,65 @@
+﻿// QG_TraningCamp_Week1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+
+
+using namespace std;
+
+
+typedef struct list_single{
+	int v;
+	struct list_single* next;
+} LinkList_single;
+
+LinkList_single* createSingleList(int n) {
+	LinkList_single* head, *curNode, *preNode;
+
+	if (n <= 0)	return NULL;
+
+	head = (LinkList_single*)malloc(sizeof(LinkList_single));
+	curNode = (LinkList_single*)malloc(sizeof(LinkList_single));
+	head->next = curNode;
+	curNode->next = NULL;
+	curNode->v = 1;
+
+	if (n == 1) {
+		return head;
+	}
+
+	for (int i = 1; i < n; i++) {
+		preNode = curNode;
+		curNode = (LinkList_single*)malloc(sizeof(LinkList_single)); 
+		preNode->next = curNode;
+		curNode->next = NULL;
+		curNode->v = i + 1;
+	}
+
+	return head;
+}
+
+void ListOutput(LinkList_single* head) {
+	LinkList_single* node = head;
+	if (!node || !node->next)	return;
+
+	node = node->next;
+	while (node != NULL) {
+		printf("%d ", node->v);
+		node = node->next;
+	}
+
+	putchar(10);
+
+	return;
+}
+
+int main()
+{
+	LinkList_single* headSingle;
+	int n;
+	scanf("%d", &n);
+
+	headSingle = createSingleList(n);
+	ListOutput(headSingle);
+
+	return 0;
+}
---
+++ Week 1/LargeGroup/DuLinkedlist/Sources/DulinkedList.cpp
@@ -0,0 +1,41 @@
+﻿#include "DulinkedList.h";
+
+
+int main() {
+	ElemType a;
+	DuLinkedList head;
+
+	if (!InitList_DuL(&head))
+		cout << "Get headNode fail" << "\n";
+
+	while (cin >> a) {//插入新节点，并测试InsertAfterList_DuL函数,CTRL + Z结束
+		DuLinkedList newnode = (DuLinkedList)malloc(sizeof(DuLNode));
+		newnode->data = a;
+		InsertAfterList_DuL(head, newnode);
+	}
+
+	//检查输入的数据,并测试TraverseList_DuL函数
+	TraverseList_DuL(head, printNodeEl);
+	putchar(10);
+
+	//测试DeleteList_DuL函数
+	DeleteList_DuL(head, &a);
+	TraverseList_DuL(head, printNodeEl);
+	putchar(10);
+
+	//测试InsertBeforeList_DuL函数
+	DuLinkedList newnode = (DuLinkedList)malloc(sizeof(DuLNode));
+	newnode->data = a;
+	InsertBeforeList_DuL(head->next, newnode);
+	
+	TraverseList_DuL(head, printNodeEl);
+	putchar(10);
+
+	//执行DestroyList_DuL函数
+	DestroyList_DuL(&head);
+	cout << "The list is destroyed successfully" << "\n";
+
+	system("pause");
+
+	return 0;
+}
